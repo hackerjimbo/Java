c3b47d81e4f993ef290bff946da25238a7eb4735
Jim Darby <uberscubajim@gmail.com>
2016-04-18 20:23:23 +0100

    Epic update

--- WS2811/WS2811.java
+++ WS2811/WS2811.java
@@ -24,12 +24,11 @@
  */
 public class WS2811 {
     public WS2811 (int width, int height,
-            boolean flip_x,
-            boolean flip_y,
-            boolean snake,
-            int type, double brightness)
+		   Mapping map,
+		   int type,
+		   double brightness)
     {
-        if (width < 0 || height < 0 ||
+        if (width <= 0 || height <= 0 ||
                 (type != WS2811Raw.WS2811_STRIP_RGB) &&
                 (type != WS2811Raw.WS2811_STRIP_RBG) &&
                 (type != WS2811Raw.WS2811_STRIP_GRB) &&
@@ -38,15 +37,49 @@ public class WS2811 {
                 (type != WS2811Raw.WS2811_STRIP_BGR) ||
                 brightness < 0 || brightness > 1)
             throw new IllegalArgumentException ("Invalid parameter to WS2811");
-        
-        width_ = width;
-        height_ = height;
-        flip_x_ = flip_x;
-        flip_y_ = flip_y;
-        snake_ = snake;
-        leds_ = width_ * height_;
+
+	i_width_ = width;
+	i_height_ = height;
+	max_ = new Point (i_width_ - 1, i_height_ - 1);
+
+	// System.out.println ("Input width " + i_width_ + " height " + i_height_ + " limit " + max_);
+	
+	final Point out = map.getOutMax ();
+	
+        o_width_ = out.getX () + 1;
+        o_height_ = out.getY () + 1;
+
+	// System.out.println ("Output width " + o_width_ + " height " + o_height_ + " limit " + out);
+		
+        leds_ = o_width_ * o_height_;
+	map_ = new int[leds_];
         data_ = new int[leds_];
-        
+
+	for (int y = 0; y < i_height_; ++y)
+	    for (int x = 0; x < i_width_; ++x) {
+		final Point p = map.map (new Point (x, y));
+
+		// System.out.println ("Mapped " + x + ',' + y + " to " + p);
+		
+		final int value = p.getX () + o_width_ * p.getY ();
+
+		// System.out.println ("Map " + x + ',' + y + " to " + value + " at " + (x + i_width_ * y));
+		
+		map_[x + i_width_ * y] = value;
+	    }
+
+	/* System.out.println ();
+
+	for (int i = 0; i < leds_; ++i) {
+	    if (i != 0)
+		System.out.print (' ');
+	    
+	    System.out.print (map_[i]);
+	}
+
+	System.out.println ();
+	*/
+	
         for (int i = 0; i < leds_; ++i)
             data_[i] = 0;
         
@@ -55,22 +88,18 @@ public class WS2811 {
         WS2811Raw.ws2811_update (data_);
     }
     
-    public void setPixel (int x, int y, int r, int g, int b)
+    public void setPixel (Point p, int r, int g, int b)
     {       
-         if (x < 0 || x>= width_ || y < 0 || y >= height_ ||
+	if (!p.inside (max_) ||
                 r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b >= 255)
             throw new IllegalArgumentException ("Invalid parameter to WS2811.setPixel");
-        
-        if (flip_x_)
-            x = (width_ - 1) - x;
-        
-        if (flip_y_)
-            y = (height_ - 1) - y;
-        
-        if (snake_ && (y & 1) != 0)
-            x = (width_ - 1) - x;
-        
-        data_[x + width_ * y] = (r << 16) | (g << 8) | b;
+
+	final int x = p.getX ();
+	final int y = p.getY ();
+	
+	// System.out.println ("pos " + x + ',' + y + " -> " + map_[x + i_width_ * y]);
+	 
+        data_[map_[x + i_width_ * y]] = (r << 16) | (g << 8) | b;
     }
     
     public void show ()
@@ -83,11 +112,15 @@ public class WS2811 {
         WS2811Raw.ws2811_close ();
     }
     
-    final private int width_;
-    final private int height_;
+    final private int i_width_;
+    final private int i_height_;
+    final private Point max_;
+    
+    final private int o_width_;
+    final private int o_height_;
+    
     final private int leds_;
-    final private boolean flip_x_;
-    private final boolean flip_y_;
-    final private boolean snake_;
+
+    final private int[] map_;
     final private int[] data_;
 }
