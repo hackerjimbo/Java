8233a80f70b6661c679a6ef611a27c042d755248
Jim Darby <uberscubajim@gmail.com>
2016-04-21 20:41:02 +0100

    Went on a commenting frenzy.

--- WS2811/Identity.java
+++ WS2811/Identity.java
@@ -1,29 +1,57 @@
+/**
+ * A Mapping that leaves everything unchanged: the Identity mapping.
+ */
+
 public class Identity extends Mapping
 {
+    /**
+     * Create a simple case with a width and a height.
+     * @param width The width.
+     * @param height The height.
+     */
     public Identity (int width, int height)
     {
 	super (new Point (width - 1, height - 1));
     }
 
+    /**
+     * Create a mapping base on a previous one. We output the same dimensions as
+     * we input.
+     * @param before 
+     */
     public Identity (Mapping before)
     {
 	super (before, before.getOutMax ());
     }
     
+    /**
+     * Perform the mapping.
+     * @param p The point to map.
+     * @return The mapped point.
+     */
     public Point map (Point p)
     {
+        // If there is someone before us then run their mapping.
 	if (before_ != null)
 	    p = before_.map (p);
 	
+        // Validate the input AFTER any previous mapping.
 	validateIn (p);
 
+        // Perform the mapping.
 	final Point result = p;
 
+        // Check the output is valud (yes, we're paranoid).
 	validateOut (result);
 	
+        // And return it.
 	return result;
     }
 
+    /**
+     * Return a string description of the mapping.
+     * @return The description.
+     */
     public String toString ()
     {
 	String result = "Identity from " + getInMax () + " to " + getOutMax ();
@@ -34,4 +62,3 @@ public class Identity extends Mapping
 	return result;
     }
 }
-
