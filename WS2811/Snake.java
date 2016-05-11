f52053bc1e27cc7fd905d51e438412e36728bfe4
Jim Darby <uberscubajim@gmail.com>
2016-05-11 15:56:02 +0100

    Moved to LGPL v3.

--- WS2811/Snake.java
+++ WS2811/Snake.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016 Jim Darby.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>..
+ */
+
 /**
  * The snake transformation. Some boards (like the Pimoroni Unicorn HAT) wire
  * the LEDs in a snake like formation so that we have to exchange the X
